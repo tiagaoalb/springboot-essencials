320ec18f04c26a97418f7dfae88f775e4f97d273
Tiago <tiagaoalb@gmail.com>
2023-11-09 11:18:00 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot.controller;
+
+import academy.devdojo.springboot.domain.Anime;
+import academy.devdojo.springboot.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+    }
+}
