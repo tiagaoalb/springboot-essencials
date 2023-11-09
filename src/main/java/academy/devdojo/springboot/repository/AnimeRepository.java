e32769d16fb06d63cab18539ae62beb370177749
Tiago <tiagaoalb@gmail.com>
2023-11-09 19:02:06 -0300

    chore: add AnimeRepository

---
+++ src/main/java/academy/devdojo/springboot/repository/AnimeRepository.java
@@ -0,0 +1,10 @@
+package academy.devdojo.springboot.repository;
+
+import academy.devdojo.springboot.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
+    List<Anime> findById();
+}
