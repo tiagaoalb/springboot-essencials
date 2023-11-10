68e03cc51a4b67a0a64128b1b1a02f91c3e0e14d
Tiago <tiagaoalb@gmail.com>
2023-11-10 16:53:55 -0300

    chore: add AnimeMapper

---
+++ src/main/java/academy/devdojo/springboot/mapper/AnimeMapper.java
@@ -0,0 +1,17 @@
+package academy.devdojo.springboot.mapper;
+
+import academy.devdojo.springboot.domain.Anime;
+import academy.devdojo.springboot.requests.AnimePostRequestBody;
+import academy.devdojo.springboot.requests.AnimePutRequestBody;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public abstract class AnimeMapper {
+
+    public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+
+    public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
+
+    public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
+}
